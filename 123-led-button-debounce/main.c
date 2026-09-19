9f132d5bb14492585b9e3d819ff2fc3ca30907f1
Виктория Сахно <victoriassnnnn@gmail.com>
2026-09-19 16:43:03 +0300

    Сделана п1.2.3

--- 123-led-button-debounce/main.c
+++ 123-led-button-debounce/main.c
@@ -5,9 +5,12 @@
 const uint LED_PIN = 25;
 const uint BUTTON_PIN = 15;
 
-volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
-volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
-const uint32_t led_mask = 1u << LED_PIN;
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
 
 int main()
 {
@@ -33,4 +36,3 @@ int main()
 
         previous = current;
     }
-}
