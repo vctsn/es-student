de80dc1d4dd975fa9ea0ffb9723175cf47f18f47
Виктория Сахно <victoriassnnnn@gmail.com>
2026-09-19 16:47:53 +0300

    Сделана п1.2.3

--- 123-led-button-debounce/main.c
+++ 123-led-button-debounce/main.c
@@ -26,7 +26,7 @@ int main()
 
     while(1)
     {
-        bool current = gpio_get(BUTTON_PIN);
+        bool current = get_button_debounce(BUTTON_PIN);
 
         if (previous == true && current == false)
         {
