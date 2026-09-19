58708111e99b1b18e97c8500bdf6a501af3691b8
Виктория Сахно <victoriassnnnn@gmail.com>
2026-09-19 16:41:58 +0300

    Сделана п1.2.3

--- 122-led-button/main.c
+++ 122-led-button/main.c
@@ -26,7 +26,7 @@ int main()
 
     while(1)
     {
-        bool current = get_button_debounce(BUTTON_PIN);
+        bool current = gpio_get(BUTTON_PIN);
 
         if (previous == true && current == false)
         {
@@ -36,4 +36,3 @@ int main()
 
         previous = current;
     }
-}
