62bef8698e180495e2410ebdbc63c9c08b8b7162
Виктория Сахно <victoriassnnnn@gmail.com>
2026-09-26 22:45:46 +0300

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -21,6 +21,26 @@ bool get_button_debounce(uint pin)
     return state && gpio_get(pin);
 }
 
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
+
 int main()
 {
     stdio_init_all();
@@ -45,5 +65,14 @@ int main()
         }
 
         previous = current;
+
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        led = handle_command(command, led);
     }
 }
